1c3046f9f95a9ea64c91aefe44e1951333ce0719
Shehzan Mohammed <mzshehzanayub@gmail.com>
2019-02-18 13:46:26 -0500

    Add new lines after fold

--- travis_linux.sh
+++ travis_linux.sh
@@ -10,13 +10,13 @@ echo_and_run cd build
 
 echo travis_fold:start:script.cmake && echo "cmake"
 echo_and_run cmake ..
-echo travis_fold:end:script.cmake
+echo travis_fold:end:script.cmake && echo -e "\n\n"
 
 echo travis_fold:start:script.build && echo "build"
 echo_and_run make -j2
-echo travis_fold:end:script.build
+echo travis_fold:end:script.build && echo -e "\n\n"
 
 echo travis_fold:start:script.run && echo "run"
 echo_and_run ./bin/cpp-sandbox
-echo travis_fold:end:script.run
+echo travis_fold:end:script.run && echo -e "\n\n"
 
--- travis_windows.sh
+++ travis_windows.sh
@@ -10,12 +10,12 @@ echo_and_run cd build
 
 echo travis_fold:start:script.cmake && echo "cmake"
 echo_and_run cmake .. -G "Visual Studio 15 2017 Win64"
-echo travis_fold:end:script.cmake
+echo travis_fold:end:script.cmake && echo -e "\n\n"
 
 echo travis_fold:start:script.build && echo "build"
 echo_and_run cmake --build . --target ALL_BUILD --config Release
-echo travis_fold:end:script.build
+echo travis_fold:end:script.build && echo -e "\n\n"
 
 echo travis_fold:start:script.run && echo "run"
 echo_and_run ./bin/Release/cpp-sandbox
-echo travis_fold:end:script.run
+echo travis_fold:end:script.run && echo -e "\n\n"
