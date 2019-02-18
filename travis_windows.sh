939241aa5e2d4ff52a9a10e79b4df494aaff81a6
Shehzan Mohammed <mzshehzanayub@gmail.com>
2019-02-18 13:40:09 -0500

    Fix windows

--- travis_windows.sh
+++ travis_windows.sh
@@ -8,15 +8,14 @@ echo_and_run () {
 echo_and_run mkdir build
 echo_and_run cd build
 
-travis_fold start "script.cmake" && echo "cmake"
-echo_and_run cmake .. -G 'Visual Studio 15 2017 Win64'
-travis_fold end "script.cmake"
+echo travis_fold:start:script.cmake && echo "cmake"
+echo_and_run cmake .. -G "Visual Studio 15 2017 Win64"
+echo travis_fold:end:script.cmake
 
-travis_fold start "script.build" && echo "build"
+echo travis_fold:start:script.build && echo "build"
 echo_and_run cmake --build . --target ALL_BUILD --config Release
-travis_fold end "script.build"
+echo travis_fold:end:script.build
 
-travis_fold start "script.run" && echo "run"
+echo travis_fold:start:script.run && echo "run"
 echo_and_run ./bin/Release/cpp-sandbox
-travis_fold end "script.run"
-
+echo travis_fold:end:script.run
